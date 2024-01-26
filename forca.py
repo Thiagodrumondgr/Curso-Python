2681fc3e0df1cd339d826c1b13367e76cfde21c1
Thiago <Thiago.drumond.gr@gmail.com>
2024-01-25 22:11:47 -0300

    Atualizando jogo forca

--- forca.py
+++ forca.py
@@ -3,7 +3,15 @@ def jogar():
     print("**********************************")
     print("Bem vindo no jogo da Forca")
     print("**********************************")
+    
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
 
+    while (not enforcou and not acertou ):
+        chute = input("Qual letra? ")
+        print ("Jogando ...")    
 
     print("Fim do jogo")
     if (__name__ == "__main__"):
