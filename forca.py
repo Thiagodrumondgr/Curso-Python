fb13daf5c6a025eb4940281f670e98727f6555a0
Thiago <Thiago.drumond.gr@gmail.com>
2024-01-26 17:21:58 -0300

    Encerrando jogo quando o usuario acertar

--- forca.py
+++ forca.py
@@ -3,24 +3,36 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_",]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas [index] = letra
-            index = index + 1
-
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas [index] = letra
+                index = index + 1
+                
+        else:
+            erros = erros + 1
+                    
+        enforcou = erros == 6  
+        acertou  = "_" not in letras_acertadas
         print(letras_acertadas)
+        
+    if(acertou):
+        print("Você Ganhou!!")   
+    else:
+        print("Você Perdeu!!")     
 
     print("Fim do jogo")
 
