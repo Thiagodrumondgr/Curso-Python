5143bf48403ed92448b9cb775ee148fd3afcfce3
Thiago <Thiago.drumond.gr@gmail.com>
2024-01-25 23:05:10 -0300

    Acrescentando upper e strip

--- forca.py
+++ forca.py
@@ -1,26 +1,27 @@
 def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-    print("**********************************")
-    print("Bem vindo no jogo da Forca")
-    print("**********************************")
-    
     palavra_secreta = "banana"
-    
+
     enforcou = False
     acertou = False
 
-    while (not enforcou and not acertou):
+    while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
+        chute = chute.strip()
 
         index = 0
-    for letra in palavra_secreta:
-        if (chute == letra):
-            print("Encontrei a letra {} na posição {}".format(chute, index))
-        index = index + 1
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
 
-    print("Jogando...")  
+        print("jogando ...")
 
     print("Fim do jogo")
-    if (__name__ == "__main__"):
-        jogar()
+
+if(__name__ == "__main__"):
+    jogar()
