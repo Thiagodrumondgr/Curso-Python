629b838c4ae3f607d63f313db31a089c70375453
Thiago <Thiago.drumond.gr@gmail.com>
2024-01-27 16:24:48 -0300

    Organizando o codigo em funçoes

--- forca.py
+++ forca.py
@@ -1,23 +1,14 @@
 
 import random
+
+
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
     
-    arquivo = open("palavras.txt", "r")
-    palavras = []
+    imprimir_mensagem_abertura()
     
-    for linha in arquivo:
-            linha = linha.strip()
-            palavras.append(linha)
-        
-    arquivo.close()
+    palavra_secreta = carrega_palavra_secreta()
     
-    numero =random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-  
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -50,6 +41,32 @@ def jogar():
         print("Você Perdeu!!")     
 
     print("Fim do jogo")
+
+
+    
+def imprimir_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    
+    for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+        
+    arquivo.close()
+    
+    numero =random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    
+    return ["_" for letra in palavra]
+
 
 if(__name__ == "__main__"):
     jogar()
