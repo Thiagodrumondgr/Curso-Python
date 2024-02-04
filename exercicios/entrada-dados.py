2fe4165956a94478ab524da00711199e6d4eb5fc
ThiagoDrumond <137659083+Thiagodrumondgr@users.noreply.github.com>
2024-02-04 11:47:30 -0300

    Acrescentando condicional no exercicio

--- exercicios/entrada-dados.py
+++ exercicios/entrada-dados.py
@@ -1,7 +1,15 @@
 funcionario1 = input("Digite o nome do funcionario: ")
-salario1 = float(input("Digite seu salario"))
-funcionario2 = input("Digite o nome do funcionario")
-salario2 = float(input("Digite seu salario"))
+salario1 = float(input("Digite seu salario: "))
+funcionario2 = input("Digite o nome do funcionario: ")
+salario2 = float(input("Digite seu salario: "))
+if salario1 >= salario2:
+    maior_salario = funcionario1
+else:
+    maior_salario = funcionario2
 
 print (f"O nome do funcionario 1 é {funcionario1}")
-print(f"O salario do funcionario 1 é de {salario1:.2}")
+print(f"O salario do funcionario 1 é de {salario1:.2f}")
+
+print (f"O nome do funcionario 2 é {funcionario2}")
+print(f"O salario do funcionario 2 é de {salario2:.2f}")
+print(f"O Funcionario que tem o maior salario é o {maior_salario}")
