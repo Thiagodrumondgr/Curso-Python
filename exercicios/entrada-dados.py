1ebaa44b3b6d212d6ea6cf1077700a12d5c9a356
ThiagoDrumond <137659083+Thiagodrumondgr@users.noreply.github.com>
2024-02-04 10:52:24 -0300

    Entrada de dados e saida de dados

---
+++ exercicios/entrada-dados.py
@@ -0,0 +1,7 @@
+funcionario1 = input("Digite o nome do funcionario: ")
+salario1 = float(input("Digite seu salario"))
+funcionario2 = input("Digite o nome do funcionario")
+salario2 = float(input("Digite seu salario"))
+
+print (f"O nome do funcionario 1 é {funcionario1}")
+print(f"O salario do funcionario 1 é de {salario1:.2}")
---
+++ exercicios/saida-dados.py
@@ -0,0 +1,15 @@
+"""nome= "thiago"
+sexo = "M"
+salario = 8580.158
+funcao = "programador"
+
+print (f"O funcionario {nome} do sexo {sexo} que trabalha na função de {funcao} atualmente ganha o salario de {salario:.2f}")
+"""
+nome = "Maria"
+cargo = "QA"
+salario = 11568.123
+sexo = "F"
+ProporcionalDeciomo = salario / 12
+
+print (f"A funcionaria {nome} do sexo {sexo} que atualmente esta no cargo de {cargo} e recebe o salario de {salario:.2f}")
+print(f"Seu decimo terceiro dividido por mes seria {ProporcionalDeciomo:.2f} ")
